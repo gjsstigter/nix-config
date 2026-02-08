e34f80bb9170a1c88097c8291d992bda31dcfd14
Stèlios Stigter <sstigter@tilaa.com>
2026-02-08 20:09:10 +0100

    TMUX conf

--- home/programs/ghostty.nix
+++ home/programs/ghostty.nix
@@ -2,6 +2,15 @@
 
 {
   programs.ghostty = {
-    enable = true;
+  enable = true;
+  package = null;  # Don't install via nix, use brew version
+  settings = {
+    theme = "catppuccin-mocha";
+    font-family = "JetBrains Mono";
+    font-size = 13;
+    window-padding-x = 10;
+    window-padding-y = 10;
+    # Add other settings here
   };
+    };
 }
