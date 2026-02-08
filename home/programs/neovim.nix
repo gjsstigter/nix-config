e32cddb90b0af7a3a0d32856aac5b9974bf85f44
Stèlios Stigter <sstigter@tilaa.com>
2026-02-08 20:36:00 +0100

    Added neovim config

--- home/programs/neovim.nix
+++ home/programs/neovim.nix
@@ -1,18 +1,34 @@
 { config, pkgs, ... }:
 
 {
-  programs.neovim = {
-  enable = true;
-  extraPackages = with pkgs; [
-        # LazyVim
-#        lua-language-server
-#        stylua
-        # Telescope
-#        ripgrep
-      ];
+    programs.neovim = {
+      enable = true;
+      defaultEditor = true;
+      viAlias = true;
+      vimAlias = true;
 
-      plugins = with pkgs.vimPlugins; [
-        lazy-nvim
-      ];
-  };
+      extraLuaConfig = ''
+        -- Bootstrap lazy.nvim
+        local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+        if not vim.loop.fs_stat(lazypath) then
+          vim.fn.system({
+            "git",
+            "clone",
+            "--filter=blob:none",
+            "https://github.com/folke/lazy.nvim.git",
+            "--branch=stable",
+            lazypath,
+          })
+        end
+        vim.opt.rtp:prepend(lazypath)
+
+        -- Configure lazy.nvim
+        require("lazy").setup({
+          -- Your plugins here
+          { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+          { "neovim/nvim-lspconfig" },
+          -- Add more plugins as needed
+        })
+      '';
+    };
 }
